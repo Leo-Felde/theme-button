efc2a3e2f0699f08e29c0ab0d408312ddf4a1341
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-06-15 15:59:29 -0300

    botão para mudar o tema

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="wrapper"
+    :class="{'theme--dark' : themeDark}"
+  >
+    <ThemeButton @toggle-theme="changeTheme" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+import ThemeButton from './components/ThemeButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ThemeButton
+  },
+
+  setup () {
+    const themeDark = ref(false)
+
+    const changeTheme = () => {
+      themeDark.value = !themeDark.value
+    }
+
+    return {
+      themeDark,
+      changeTheme
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "./styles/main.sass"
+
+.wrapper
+  width: 100%
+  height: 100%
+  display: flex
+  background: #ced6d9
+  transition: background-color 500ms linear
+  &.theme--dark
+    background: rgb(29, 39, 51) !important
+
+#theme-button
+  margin-left: auto
+  margin-right: auto
+  margin-top: auto
+  margin-bottom: auto
+</style>
+
